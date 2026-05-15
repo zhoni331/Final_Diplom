b268a066737c9a8d22a314a09079e4c199c776e7
zhoni331 <zanibektljsibekov341@gmail.com>
2026-05-15 20:38:34 +0600

    hosting map fix

--- diplom_project/diplom_project/settings.py
+++ diplom_project/diplom_project/settings.py
@@ -36,6 +36,8 @@ ALLOWED_HOSTS = [
     ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = False  # только если хочешь редирект
+USE_X_FORWARDED_HOST = True
 
 USE_X_FORWARDED_HOST = True
 
@@ -174,3 +176,34 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.security': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
