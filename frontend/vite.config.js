90db057e2df171e5901e6682c40e7e74524a9864
zhoni331 <zanibektljsibekov341@gmail.com>
2026-05-15 20:58:46 +0600

    hosting fix 0.0.3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true, // Включаем генерацию source map
 })
